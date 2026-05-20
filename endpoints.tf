4f072005beb8dc323366d7e372f2bd3da92267ef
Bikash-cloud <jha.bikash86@gmail.com>
2026-05-20 15:35:28 +0800

    networking: add s3 vpc gateway endpoint for private subnet routing

---
+++ endpoints.tf
@@ -0,0 +1,16 @@
+# Create the S3 VPC Gateway Endpoint
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.ap-southeast-1.s3"
+  vpc_endpoint_type = "Gateway"
+
+  tags = {
+    Name = "s3-vpc-endpoint"
+  }
+}
+
+# Attach the Endpoint to your Private Route Table
+resource "aws_vpc_endpoint_route_table_association" "private_s3" {
+  vpc_endpoint_id = aws_vpc_endpoint.s3.id
+  route_table_id  = aws_route_table.private.id
+}
