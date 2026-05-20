081b5373b4503d8e7ea3fe551b1f712d0550a86a
Bikash-cloud <jha.bikash86@gmail.com>
2026-05-20 16:38:08 +0800

    security: add s3 bucket policy to allow cloudfront oac access

---
+++ s3_policy.tf
@@ -0,0 +1,24 @@
+# Attach a Bucket Policy to allow CloudFront OAC to read files
+resource "aws_s3_bucket_policy" "allow_cloudfront" {
+  bucket = aws_s3_bucket.assets.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "AllowCloudFrontServicePrincipalReadOnly"
+        Effect    = "Allow"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+        Action   = "s3:GetObject"
+        Resource = "${aws_s3_bucket.assets.arn}/*"
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.cdn.arn
+          }
+        }
+      }
+    ]
+  })
+}
